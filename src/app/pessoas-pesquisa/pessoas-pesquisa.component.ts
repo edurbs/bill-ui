9dc86a7bb33d29ff5aaaeb4a1388943faa3a3478
Eduardo Soares <edurbs@gmail.com>
2023-12-16 21:44:22 -0300

    add component search for person

---
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas-pesquisa',
+  templateUrl: './pessoas-pesquisa.component.html',
+  styleUrls: ['./pessoas-pesquisa.component.css'],
+})
+export class PessoasPesquisaComponent implements OnInit {
+  pessoas = [
+    {
+      nome: 'Manoel Pinheiro',
+      cidade: 'Uberlância',
+      estado: 'MG',
+      ativo: true,
+    },
+    {
+      nome: 'Sebastião da Silva',
+      cidade: 'São Paulo',
+      estado: 'SP',
+      ativo: false,
+    },
+    {
+      nome: 'Carla Souza',
+      cidade: 'Florianópolis',
+      estado: 'SC',
+      ativo: true,
+    },
+    {
+      nome: 'Luís Pereira',
+      cidade: 'Curitiba',
+      estado: 'PR',
+      ativo: false,
+    },
+    {
+      nome: 'Vilmar Andrade',
+      cidade: 'Rio de Janeiro',
+      estado: 'RJ',
+      ativo: false,
+    },
+    {
+      nome: 'Eduardo Soares',
+      cidade: 'Camaducaia',
+      estado: 'MG',
+      ativo: true,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
