db963f9faec0165d3488a5658376e064bd22523d
Eduardo Soares <edurbs@gmail.com>
2023-12-17 21:37:56 -0300

    add two-way databind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cliente {
+  nome!: string;
+  email!: string;
+  profissao!: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +14,19 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
+  cliente = new Cliente();
+
   profissoes = ['Programador', 'Empresário', 'Outra'];
+  profissaoPadrao = "Outra";
 
   salvar(usuarioForm: NgForm) {
+
+    // this.cliente.nome=usuarioForm.value.nome;
+    // this.cliente.email=usuarioForm.value.email;
+    // this.cliente.profissao=usuarioForm.value.profissao;
+
     console.log(usuarioForm.value);
+    console.log(this.cliente);
+
   }
 }
