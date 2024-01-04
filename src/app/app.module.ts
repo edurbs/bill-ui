dbe4dd38f018726a5d05bead86413de973ae0399
Eduardo Soares <edurbs@gmail.com>
2024-01-04 11:25:28 -0300

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import localePt from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
 import { CoreModule } from './core/core.module';
+import LancamentoCadastroComponent from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
 import { PessoasModule } from './pessoas/pessoas.module';
 
 
 registerLocaleData(localePt);
 
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'pessoas', component: PessoasPesquisaComponent },
+];
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, LancamentosModule, PessoasModule],
+  imports: [
+    RouterModule.forRoot(routes),
+
+    CoreModule,
+    LancamentosModule,
+    PessoasModule
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
